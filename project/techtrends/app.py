0783a3c9a0606d00df0b8f443304e9575df51308
Adam <adam@hp.probook>
2021-06-20 14:16:25 +0200

    [feature] logging & project documentation

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,3 +1,4 @@
+import logging
 import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
@@ -8,9 +9,10 @@ connections = 0
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global connections
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
-    connections + 1
+    connections = connections + 1
     return connection
 
 # Function to get a post using its ID
@@ -39,13 +41,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info('Article with id: '+str(post_id)+' not found!')
       return render_template('404.html'), 404
     else:
+      app.logger.info('Article "'+post[2]+'" retrived!')
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('Page "About us" retrived!')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -63,6 +68,7 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
+            app.logger.info('Article "'+title+'" is created!')
 
             return redirect(url_for('index'))
 
@@ -72,6 +78,14 @@ def create():
 def healthz():
     return {'result': 'OK - healthy'}
 
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT COUNT(1) FROM posts').fetchone()[0]
+
+    return {'db_connection_count': connections, 'post_count': posts}
+
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(name)s:%(asctime)s, %(message)s')
+    app.run(host='0.0.0.0', port='3111')
